308f545da359ce3cc916a218a5ad332eba2d86d3
Ricardo Kotk <rkotik@gmail.com>
2021-10-26 11:11:30 -0300

    Puesta a punto

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let mano = 0;
   let totalNosotros = 0;
   let totalEllos = 0;
-  var manogral;
+  var manogral = 0;
 
   // Variables de contexto
-  const btnCargar = document.querySelector("#btnCargar");
+  // const btnCargar = document.querySelector("#btnCargar");
   const btnBorrar = document.querySelector("#btnBorrar");
   const btnSumar = document.querySelector("#btnSumar");
   const baseNosotros = document.querySelector("#base-nosotros");
@@ -20,6 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const totalEll = document.querySelector("#te");
 
   // Funciones
+  const limpiar = () => {
+    baseDatos = [];
+    mano = 0;
+    totalNosotros = 0;
+    totalEllos = 0;
+    manogral;
+  };
+
   const sumar = (e) => {
     e.preventDefault();
     mano++; //Suma 1 al numero de manos
@@ -64,8 +72,77 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const cargar = (e) => {
+  // const cargar = (e) => {
+  //   e.preventDefault();
+  //   limpiar();
+  //   puntos = JSON.parse(localStorage.getItem("Puntos"));
+  //   if (puntos) {
+  //     let tNos = 0;
+  //     let tEllos = 0;
+  //     let mano = 0;
+  //     tabla.innerHTML = "";
+  //     puntos.forEach((puntaje) => {
+  //       tNos +=
+  //         parseInt(puntaje.baseNosotros) + parseInt(puntaje.puntosNosotros);
+  //       totalNos.textContent = Intl.NumberFormat("es-AR").format(tNos);
+  //       tEllos += parseInt(puntaje.baseEllos) + parseInt(puntaje.puntosEllos);
+  //       totalEll.textContent = Intl.NumberFormat("es-AR").format(tEllos);
+  //       mano++;
+  //       tabla.innerHTML += `
+  //         <tr>
+  //           <td>${mano}</td>
+  //           <td>${puntaje.baseNosotros}/${puntaje.puntosNosotros}</td>
+  //           <td>${puntaje.baseEllos}/${puntaje.puntosEllos}</td>
+  //         </tr>
+  //       `;
+  //       baseDatos.push({
+  //         baseNosotros: puntaje.baseNosotros,
+  //         puntosNosotros: puntaje.puntosNosotros,
+  //         baseEllos: puntaje.baseEllos,
+  //         puntosEllos: puntaje.puntosEllos,
+  //       });
+
+  //       manogral = mano;
+  //     });
+  //   } else {
+  //     error.classList.remove("hide");
+  //     error.textContent = "No hay datos para recuperar";
+  //     setTimeout(function () {
+  //       error.classList.add("hide");
+  //     }, 3000);
+  //   }
+  //   mano = manogral;
+  // };
+
+  const borrar = (e) => {
     e.preventDefault();
+    puntos = JSON.parse(localStorage.getItem("Puntos"));
+    if (!puntos) {
+      error.classList.remove("hide");
+      error.textContent = "No hay datos para eliminar";
+      setTimeout(() => {
+        error.classList.add("hide");
+      }, 3000);
+    } else {
+      confirm("¿Estás seguro de limpiar los datos del juego?");
+      localStorage.clear();
+      mano = 0;
+      totalNosotros = 0;
+      totalEllos = 0;
+      limpiar();
+      totalNos.textContent = totalNosotros;
+      totalEll.textContent = totalEllos;
+      document.querySelector("#tabla").innerHTML = "";
+      error.classList.remove("hide");
+      error.textContent = "Datos eliminados, podés empezar una nueva partida";
+      setTimeout(() => {
+        error.classList.add("hide");
+      }, 3000);
+    }
+  };
+
+  const cargarInicio = () => {
+    limpiar();
     puntos = JSON.parse(localStorage.getItem("Puntos"));
     if (puntos) {
       let tNos = 0;
@@ -95,43 +172,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
         manogral = mano;
       });
-    } else {
-      error.classList.remove("hide");
-      error.textContent = "No hay datos para recuperar";
-      setTimeout(function () {
-        error.classList.add("hide");
-      }, 3000);
     }
+    //  else {
+    //   error.classList.remove("hide");
+    //   error.textContent = "No hay datos para recuperar";
+    //   setTimeout(function () {
+    //     error.classList.add("hide");
+    //   }, 3000);
+    // }
     mano = manogral;
   };
 
-  const borrar = (e) => {
-    e.preventDefault();
-    puntos = JSON.parse(localStorage.getItem("Puntos"));
-    if (!puntos) {
-      error.classList.remove("hide");
-      error.textContent = "No hay datos para eliminar";
-      setTimeout(() => {
-        error.classList.add("hide");
-      }, 3000);
-    } else {
-      confirm("¿Estás seguro de limpiar los datos del juego?");
-      localStorage.clear();
-      mano = 0;
-      totalNosotros = 0;
-      totalEllos = 0;
-      totalNos.textContent = totalNosotros;
-      totalEll.textContent = totalEllos;
-      document.querySelector("#tabla").innerHTML = "";
-      error.classList.remove("hide");
-      error.textContent = "Datos eliminados, podés empezar una nueva partida";
-      setTimeout(() => {
-        error.classList.add("hide");
-      }, 3000);
-    }
-  };
   // Detecciones
   btnSumar.addEventListener("click", sumar);
   btnBorrar.addEventListener("click", borrar);
-  btnCargar.addEventListener("click", cargar);
+  // btnCargar.addEventListener("click", cargar);
+  cargarInicio();
 });
